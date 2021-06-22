09f5f3fad7fac0c635014a93a189b068ecaeba6e
Ryan Park <rjpark2004@gmail.com>
2021-06-21 21:32:18 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import SearchBar from './components/SearchBar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [location, setLocation] = useState(null);
+
+    const fetchData = (location) => {
+        setLocation(location);
+    }
+
+    const renderTideInfo = (location) => {
+        if (location === null) {
+            return;
+        }
+        return location.id;
+    }
+
+    return (
+        <div>
+            <SearchBar fetchData={fetchData} />
+            {renderTideInfo(location)}
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/SearchBar.js
@@ -0,0 +1,112 @@
+import React, { useReducer, useRef, useEffect } from 'react';
+import { Search } from 'semantic-ui-react';
+import _ from 'lodash';
+import stations from '../utils/stations.json';
+import states from '../utils/states.json';
+
+const locations = stations.stations;
+
+const initialState = {
+    loading: false,
+    results: [],
+    value: '',
+    location: null
+}
+
+function reducer(state, action) {
+    switch(action.type) {
+        case 'CLEAN_QUERY':
+            return initialState;
+        case 'START_SEARCH':
+            return { ...state, location: null, loading: true, value: action.query };
+        case 'FINISH_SEARCH':
+            return { ...state, loading: false, results: action.results };
+        case 'UPDATE_SELECTION':
+            return { ...state, location: action.data, results: [], value: action.selection };
+        default:
+            throw new Error();
+    }
+}
+
+const SearchBar = (props) => {
+    const [state, dispatch] = useReducer(reducer, initialState);
+    const { loading, results, value, location } = state;
+
+    const timeoutRef = useRef();
+
+    const handleSearchChange = (e, data) => {
+        clearTimeout(timeoutRef.current);
+        dispatch({ type: 'START_SEARCH', query: data.value});
+
+        timeoutRef.current = setTimeout(() => {
+            if (data.value.length === 0) {
+                dispatch({ type: 'CLEAN_QUERY' });
+                return;
+            }
+
+            const re = new RegExp(_.escapeRegExp(data.value), 'i');
+            const isMatch = (result) => re.test(result.name);
+
+            const filtered = _.filter(locations, isMatch);
+            const endIndex = Math.min(filtered.length, 5);
+
+            function renameKey( obj, oldKey, newKey ) {
+                obj[newKey] = obj[oldKey];
+                delete obj[oldKey];
+            }
+
+            var sliced = filtered.slice(0, endIndex);
+            sliced.forEach((obj) => {
+                obj.title = obj.name;
+                obj.description = states[obj.state];
+                renameKey(obj, 'tideType', 'tidetype');
+            });
+            
+            dispatch({
+                type: 'FINISH_SEARCH',
+                results: sliced
+            });
+
+        }, 300);
+    }
+
+    const onTermSubmit = (event) => {
+        event.preventDefault();
+        if (results.length === 0) {
+            return;
+        }
+        dispatch({ type: 'UPDATE_SELECTION', selection: results[0].title, data: results[0] });
+    }
+
+    useEffect(() => {
+        return () => {
+            clearTimeout(timeoutRef.current);
+        }
+    }, []);
+
+    useEffect(() => {
+        if (location === null) {
+            return;
+        }
+        props.fetchData(location);
+    }, [location, props]);
+
+    return  (
+        <form onSubmit={onTermSubmit}>
+            <Search 
+                loading={loading}
+                onResultSelect = {(e, data) => 
+                    dispatch({ type: 'UPDATE_SELECTION', selection: data.result.title, data: data.result })
+                }
+                onSearchChange={handleSearchChange} 
+                results={results}
+                value={value}
+                placeholder={"Search Locations..."}
+                onSubmit={onTermSubmit}
+                showNoResults={value.length > 0 && results.length === 0 && location === null}
+            />
+        </form>
+    );
+}
+
+export default SearchBar;
